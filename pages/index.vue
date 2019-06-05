bf06fc689500374471c94d6848ef17b29b4fb78e
MikeFranco <mfranco_98@yahoo.com>
2019-06-04 21:31:43 -0500

    project complete

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <MonsterSlayer/>
+  </div>
+</template>
+
+<script>
+import MonsterSlayer from '~/components/MonsterSlayer.vue';
+
+export default {
+  components: {
+    MonsterSlayer
+  }
+}
+</script>
